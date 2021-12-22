791f51baf56629134de6930d5fefc11f6f30c8ab
Rune Højlund <>
2021-12-22 10:39:29 +0100

    Implemented validation loss in Hyperparameter search

--- model_caching.py
+++ model_caching.py
@@ -11,11 +11,11 @@ def get_model_name(N_hid, N_layers, learning_rate, act_func, epochs):
     + '__epochs_' + str(epochs)
     + '__lr_' + str(learning_rate))
 
-def save_model(net: Net, loss_data, loss_pde, loss_bc, loss_ic, epochs, dir = './model/'):
+def save_model(net: Net, losses, epochs, dir = './model/'):
     N_hid, N_layers, learning_rate, act_func, N_params = net.get_hyperparams()
     model_path = (dir + get_model_name(N_hid, N_layers, learning_rate, act_func, epochs))
     torch.save(net.state_dict(), model_path)
-    torch.save((loss_data, loss_pde, loss_bc, loss_ic), model_path + '_losses.pt')
+    torch.save(losses, model_path + '_losses.pt')
 
     losses = np.round([loss_data[-1].item(), loss_pde[-1].item(), loss_bc[-1].item(), loss_ic[-1].item()],5)
     results_df = DataFrame([[*losses, N_params, N_hid, N_layers, str(act_func)[:-2], epochs, learning_rate]], columns=['loss_data', 'loss_pde', 'loss_bc', 'loss_ic', 'N_params', 'N_hid', 'N_layers', 'act_fun', 'epochs', 'learning_rate'])
